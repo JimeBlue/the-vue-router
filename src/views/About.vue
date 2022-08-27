2410afc557261e8bc9ac0c4e831a319399fa2620
Jimena Miguez <jimeblau@gmail.com>
2022-08-27 14:26:53 +0200

    Finish router-links

---
+++ src/views/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>This is the about page</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente fuga
+      quasi laboriosam dolor aspernatur dignissimos id unde sed quae
+      exercitationem aperiam ad cupiditate totam accusamus tempora in porro
+      minima officia blanditiis, nam eaque delectus iusto dolores. Libero, fuga
+      est odit totam, officia labore sit distinctio ut, nam velit deserunt quas
+      ducimus autem necessitatibus eaque quos dolores! Vitae quam architecto
+      aperiam blanditiis quisquam, dicta, nostrum ea dolorum deserunt totam
+      libero dignissimos quod, necessitatibus est consectetur. Perspiciatis
+      saepe distinctio vitae ut eos odio dolor? Quo placeat labore, saepe neque
+      ipsum odit nam obcaecati doloremque, dolore illo ullam atque soluta quia
+      ipsam repellendus.
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa repellat
+      voluptate repudiandae suscipit officia voluptatum, blanditiis, eligendi
+      voluptatem libero architecto, at maxime veniam? Nesciunt mollitia, commodi
+      error ab neque minima adipisci quis, aut vero sapiente esse dolor odio!
+      Officia aliquid mollitia eos, nihil labore sunt aliquam soluta quibusdam
+      ipsum sint rem placeat iste eveniet expedita. Veniam nobis reprehenderit
+      nisi exercitationem at, numquam tempora tempore laborum illo quaerat
+      accusantium vero corrupti a facilis molestiae impedit? Quae tenetur
+      repellat ab, fuga magni officia asperiores voluptates quis et vero omnis
+      quo a debitis dolorem nesciunt ratione veritatis neque aspernatur
+      accusantium. Dolor, accusantium distinctio!
+    </p>
+  </div>
+</template>
