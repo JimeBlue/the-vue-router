4e35420e9d6e4b3b55d1d3244b6f7421fb95f1a8
Jimena Miguez <jimeblau@gmail.com>
2022-08-28 11:54:58 +0200

    delete comments

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,9 +1,4 @@
 <template>
-  <!-- EXPLANATION: Imagine the router uses a path that doesn´t exist,
-a path that I didn't set in my router. I want the user to see
-a catchall 404 not found page. -->
-  <!-- 1)In folder view, create the component NotFound,
-with a 404 not found message.  The next steps are in router, index.js-->
   <div>
     <h2>404</h2>
     <h3>That page does not exist</h3>
